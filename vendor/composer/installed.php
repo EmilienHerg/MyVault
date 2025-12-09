f092259a33641cc4949e37e4f888e0d1ad23ad4a
Emilien HERGOTT <emilien.hergott@mewo-campus.fr>
2025-12-09 16:25:23 +0100

    first commit

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'vendor/app',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '79db98af2e3743610eb239bbbac2010aed01f527',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -56,9 +56,9 @@
             'dev_requirement' => false,
         ),
         'vendor/app' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '79db98af2e3743610eb239bbbac2010aed01f527',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
